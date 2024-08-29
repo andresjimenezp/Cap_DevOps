e86dda6564765d0ca3df8f7104e4876f867e8582
Andres Jimenez Palacio <Anjimenezpa>
2024-08-29 14:47:39 -0500

    feat: Agregar DockerFile y Cambios en Main

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from datetime import datetime
-from typing import List
+import json
 
 app = FastAPI()
 
+
 @app.get("/lista-ordenada")
-def lista_ordenada(lista_no_ordenada: List[int] = Query(...)):
+def lista_ordenada(lista_no_ordenada: str):
+    lista = [int(x) for x in lista_no_ordenada.split(",")]
+
     hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    lista_ordenada = sorted(lista_no_ordenada)
-    return {"hora_sistema": hora_actual, "lista_ordenada": lista_ordenada}
+    lista_ordenada = sorted(lista)
+
+    response = {
+        "hora_sistema": hora_actual,
+        "lista_ordenada": lista_ordenada
+    }
+
+    return json.loads(json.dumps(response, indent=4))
+
 
 @app.get("/healthcheck")
 def healthcheck():
