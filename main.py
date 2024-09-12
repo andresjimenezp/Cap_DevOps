46680532c9ec7bbb42e58102b7c3df33de491f9b
Andres Jimenez Palacio <Anjimenezpa>
2024-09-12 09:40:26 -0500

    feat: Agregar el endpoint para guardar lista no ordenada

--- main.py
+++ main.py
@@ -2,35 +2,42 @@ from fastapi import FastAPI
 from datetime import datetime
 import pytz
 import json
+from mongo import guardar_mongo_db  # Importar la función
 
-# Crear una instancia de la aplicación FastAPI
 app = FastAPI()
 
-# Definir un endpoint GET en la ruta "/lista-ordenada"
+# Endpoint para ordenar una lista
 @app.get("/lista-ordenada")
 def lista_ordenada(lista_no_ordenada: str):
-    # Convertir la lista de números que se pasa como una cadena de texto en una lista de enteros
     lista = [int(x) for x in lista_no_ordenada.split(",")]
 
-    # Obtener la zona horaria de Colombia
     zona_horaria_colombia = pytz.timezone("America/Bogota")
-    # Obtener la hora actual en la zona horaria de Colombia y formatearla como una cadena de texto
     hora_actual = datetime.now(zona_horaria_colombia).strftime("%Y-%m-%d %H:%M:%S")
 
-    # Ordenar la lista de números
     lista_ordenada = sorted(lista)
 
-    # Crear un diccionario con la hora actual y la lista ordenada
     respuesta = {
         "hora_sistema": hora_actual,
         "lista_ordenada": lista_ordenada
     }
 
-    # Convertir el diccionario a formato JSON y devolverlo como respuesta
     return json.loads(json.dumps(respuesta, indent=4))
 
-# Definir un endpoint GET en la ruta "/healthcheck" para verificar si la API está activa
+# Nuevo endpoint para guardar la lista no ordenada en MongoDB
+@app.get("/guardar-lista-no-ordenada")
+def guardar_lista_no_ordenada(lista_no_ordenada: str):
+    lista = [int(x) for x in lista_no_ordenada.split(",")]
+
+    zona_horaria_colombia = pytz.timezone("America/Bogota")
+    hora_actual = datetime.now(zona_horaria_colombia).strftime("%Y-%m-%d %H:%M:%S")
+
+    # Guardar la lista en MongoDB usando la función importada
+    id_unico = guardar_mongo_db(lista, hora_actual)
+
+    # Respuesta de éxito con el ID único
+    return {"msg": f"La lista no ordenada fue guardada con el id: {id_unico}"}
+
+# Endpoint para verificar el estado del API
 @app.get("/healthcheck")
 def healthcheck():
-    # Devolver el estado "OK" como una respuesta simple
     return "OK"
